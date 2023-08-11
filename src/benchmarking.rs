e987f5820be0ffa24c045adc342bc0d3413bd6b3
Reshetov Denis <reshetovdenis@gmail.com>
2023-08-11 10:12:28 +0200

    Fixing missing arguments

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -19,25 +19,14 @@ pub const EMPLOYER_ID: [u8; 32] = [
 
 benchmarks! {
 	reimburse {
-		let referee_signature: [u8; 64] = [
-			230, 177, 124,  45,  11,  85,  71, 101, 190, 120, 239,
-			211, 218, 219, 206, 153, 133,  14, 144,  52, 254, 192,
-			 75, 116, 222,  90, 104,  63, 233, 254,  44,  94, 183,
-			 94,  42, 222, 150, 253, 223,  27,  94,  18, 136, 186,
-			156,   7, 169, 225, 232, 146, 222, 190, 131,  61, 224,
-			141,  28,   4,  46, 133, 237, 155,  57, 128,
-		];
-		let worker_signature: [u8; 64] = [
-			102,  45, 124, 211,  15, 227, 227, 231,  97,  99,  98,
-			123, 213, 172, 167,   4, 145, 194, 184,  62,  59,  62,
-			140,  12,  49,  95, 197, 236,  83,  35,  56, 119, 136,
-			26, 240, 141,  63,  17,  81, 157, 120,  64, 194,  80,
-			140, 247,   8, 108, 183, 107,  18,  74,  42, 114, 252,
-			138,  15, 232,  13,  62,  73, 235, 204, 138,
-		];
+		let referee_signature: [u8; 64] = [216,163,30,143,81,31,127,209,105,35,237,107,180,150,128,121,166,124,79,247,98,190,97,211,154,50,146,127,246,177,57,5,204,56,21,52,78,158,254,146,128,63,126,181,50,45,46,9,96,32,200,236,244,25,100,169,213,236,67,172,140,66,232,139];
+        let worker_signature: [u8; 64] = [168,174,81,192,173,33,161,63,219,108,119,65,205,98,248,17,248,180,216,88,217,168,129,79,11,151,82,9,19,250,17,95,145,12,117,145,145,6,96,37,240,65,79,8,179,109,8,110,110,215,221,35,100,45,219,34,170,196,28,17,68,102,111,135];
+        
 
 		let caller = whitelisted_caller();
-		let letter_id = 0 as u32;
+		let para_id = 1 as u32;
+		let letter_id = 1 as u32;
+		let block_number = 100 as u64;
 		let ask_price: BalanceOf<T> = 0u32.into();
 
 		let referee_id = H256::from(REFEREE_ID);
@@ -45,7 +34,7 @@ benchmarks! {
 		let employer_id = H256::from(EMPLOYER_ID);
 		let referee_sign = H512::from(referee_signature);
 		let worker_sign = H512::from(worker_signature);
-	}: _(RawOrigin::Signed(caller), letter_id, referee_id, worker_id, employer_id, ask_price, referee_sign, worker_sign)
+	}: _(RawOrigin::Signed(caller), para_id, letter_id, block_number, referee_id, worker_id, employer_id, ask_price, referee_sign, worker_sign)
 }
 
 impl_benchmark_test_suite!(Pallet, crate::tests::new_test_ext(), crate::tests::Test,);
